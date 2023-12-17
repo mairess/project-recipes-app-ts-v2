8f5619e8ad27a68ed3172877fb02c950d0b36800
mairess <trabalho.maires@gmail.com>
2023-12-17 08:53:09 -0300

    feat: redirect user to profile page

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,34 +1,58 @@
-import { useLocation } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import profileIcon from '../../images/profileIcon.svg';
 import searchIcon from '../../images/searchIcon.svg';
 import usePageTitle from '../../hooks/usePageTitle';
+import logoBell from '../../images/icon-Bell.svg';
+import logoText from '../../images/icon-text.svg';
+import iconPlate from '../../images/icon-plate.svg';
+import { Container, Title, WrapperTitle } from './style';
 
 function Header() {
   const { pathname } = useLocation();
   const { title } = usePageTitle(pathname);
   return (
     <div>
-      <img
-        src={ profileIcon }
-        data-testid="profile-top-btn"
-        alt="Profile Icon"
-        width="31"
-        height="31"
-      />
+      <Container>
+        <div>
+          <img
+            src={ logoBell }
+            alt="Logo bell"
+            width="42.6"
+            height="39.2"
+          />
+          <img src={ logoText } alt="Logo text" />
+        </div>
+        <div>
+          <Link to="/profile">
+            <img
+              src={ profileIcon }
+              data-testid="profile-top-btn"
+              alt="Profile Icon"
+              width="28.71"
+              height="28.71"
+            />
+          </Link>
+          {(title === 'Meals' || title === 'Drinks') && (
+            <img
+              src={ searchIcon }
+              data-testid="search-top-btn"
+              alt="Search Icon"
+              width="26.8"
+              height="26.8"
+            />
+          )}
+        </div>
+      </Container>
 
-      {(title === 'Meals' || title === 'Drinks') && (
+      <WrapperTitle>
         <img
-          src={ searchIcon }
-          data-testid="search-top-btn"
-          alt="S earch Icon"
-          width="31"
-          height="31"
+          src={ iconPlate }
+          alt="Icon plate"
         />
-      )}
-
-      <h1 data-testid="page-title">
-        {title}
-      </h1>
+        <Title data-testid="page-title">
+          {title}
+        </Title>
+      </WrapperTitle>
     </div>
   );
 }
---
+++ src/components/header/style.ts
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 22.5rem;
+    height: 3.25rem;
+    flex-shrink: 0;
+    background: #FCDC36;
+    padding: 0 1rem;
+
+    div {
+        display: flex;
+        gap: 1rem;
+    }
+`;
+
+export const WrapperTitle = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 1rem;
+    margin-top: 2rem;
+`;
+
+export const Title = styled.h1`
+    width: 9.1875rem;
+    color: #41197F;
+    text-align: center;
+    font-size: 1.25rem;
+    font-style: normal;
+    font-weight: 900;
+    line-height: normal;
+    letter-spacing: 0.13125rem;
+    text-transform: uppercase;
+`;
