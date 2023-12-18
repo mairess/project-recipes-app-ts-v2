adff1dfa77e43a320e051e66f36abd092269c15c
mairess <trabalho.maires@gmail.com>
2023-12-18 19:58:22 -0300

    feat: fetch drinks

---
+++ src/endpoints.ts
@@ -0,0 +1,6 @@
+export const API_MEALS_BY_INGREDIENT = 'https://www.themealdb.com/api/json/v1/1/filter.php?i=';
+export const API_MEALS_BY_NAME = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
+export const API_MEALS_BY_LETTER = 'https://www.themealdb.com/api/json/v1/1/search.php?f=';
+export const API_DRINKS_BY_INGREDIENT = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=';
+export const API_DRINKS_BY_NAME = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+export const API_DRINKS_BY_LETTER = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=';
--- src/hooks/useFetchMeals.ts
+++ src/hooks/useFetchMeals.ts
@@ -1,29 +1,37 @@
 import { useContext, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import getEndpoint from '../helpers/getEndpoint';
 import FoodContext from '../context/FoodContext';
 
 const useFetchMeals = (termToSearch: string) => {
   const { isButtonClicked, filter } = useContext(FoodContext);
+  const { pathname } = useLocation();
+  const pagePath = pathname.slice(1);
+
   let endpoint: string;
 
   switch (filter) {
     case 'Ingredient':
-      endpoint = `https://www.themealdb.com/api/json/v1/1/filter.php?i=${termToSearch}`;
+      endpoint = getEndpoint(termToSearch, pathname).ENDPOINT_BY_INGREDIENT;
       break;
     case 'Name':
-      endpoint = `https://www.themealdb.com/api/json/v1/1/search.php?s=${termToSearch}`;
+      endpoint = getEndpoint(termToSearch, pathname).ENDPOINT_BY_NAME;
       break;
     case 'First Letter':
-      endpoint = `https://www.themealdb.com/api/json/v1/1/search.php?f=${termToSearch}`;
+      endpoint = getEndpoint(termToSearch, pathname).ENDPOINT_BY_LETTER;
       break;
     default:
-      endpoint = `https://www.themealdb.com/api/json/v1/1/filter.php?i=${termToSearch}`;
+      endpoint = getEndpoint(termToSearch, pathname).ENDPOINT_BY_INGREDIENT;
       break;
   }
 
   useEffect(() => {
     const getTheMeals = async () => {
       const data = await fetch(endpoint);
-      const meals = await data.json();
+      const content = await data.json();
+      console.log(content[pagePath]);
+
+      return content[pagePath];
     };
 
     if (filter === 'First Letter' && termToSearch.length > 1) {
