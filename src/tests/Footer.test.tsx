383a134ee79ab193e824a399768768a44450f55e
mairess <trabalho.maires@gmail.com>
2023-12-22 07:42:49 -0300

    test: Footer component

---
+++ src/tests/Footer.test.tsx
@@ -0,0 +1,23 @@
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from './utils/renderWithRouter';
+
+describe('Testing Footer component.', () => {
+  test('Verify if Footer and its buttons are loaded and if they navigate to routes correctly.', async () => {
+    const { user } = renderWithRouter(<App />, { route: '/meals' });
+
+    const footer = screen.getByTestId('footer');
+    const drinkBtn = screen.getByTestId('drinks-bottom-btn');
+    const mealBtn = screen.getByTestId('meals-bottom-btn');
+
+    expect(footer).toBeVisible();
+    expect(drinkBtn).toBeVisible();
+    expect(mealBtn).toBeVisible();
+
+    await user.click(drinkBtn);
+    expect(window.location.pathname).toBe('/drinks');
+
+    await user.click(mealBtn);
+    expect(window.location.pathname).toBe('/meals');
+  });
+});
