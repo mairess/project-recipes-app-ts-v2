87da0bb89b5ae1de2a189687a4686daae662c6cf
mairess <trabalho.maires@gmail.com>
2023-12-24 19:13:23 -0300

    rename: Recommended

---
+++ src/components/Recommended/style.ts
@@ -0,0 +1,45 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    display: flex;
+    align-items: center;
+    width: 21.02056rem;
+    height: 12.20088rem;
+    flex-shrink: 0;
+    overflow: scroll;
+
+    p {
+        color: #1A1B1C;
+        font-size: 0.75rem;
+        font-style: normal;
+        font-weight: 400;
+        line-height: normal;
+        margin: 0.5rem 0 0.5rem 1rem;
+    }
+`;
+
+export const Title = styled.h1`
+    color: #1A1B1C;
+    font-size: 1.25rem;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+    margin: 1rem 0 0.6rem 1rem;
+`;
+
+export const WrapperImage = styled.div`
+    width: 10;
+    height: 10.38238rem;
+    flex-shrink: 0;
+    border-radius: 0.3125rem;
+    border: 0.518px solid #B1B1B1;
+    background: #FFF;
+    margin: 0.25rem;
+`;
+
+export const Img = styled.img`
+    width: 9.9rem;
+    height: 8.4375rem;
+    flex-shrink: 0;
+    border-radius: 0.3125rem 0.3125rem 0rem 0rem;
+`;
